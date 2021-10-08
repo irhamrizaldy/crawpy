f8273e28b218097a493ddeb9d583fc29fa84d03d
irhamrizaldy <43570194+irhamrizaldy@users.noreply.github.com>
2021-10-08 15:25:09 +0700

    handling input form on the back-end

--- crawpy.py
+++ crawpy.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask import render_template
+from flask import request #This imports the request module from Flask, which allows us to easily handle many aspects of HTTP requests, including data sent via forms.
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.form:
+        print(request.form)
     # return "My Flask app"
-    return render_template("index.html")
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(host='localhost', debug=True)
